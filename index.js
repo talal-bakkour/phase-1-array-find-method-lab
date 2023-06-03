b6d44a25f468329129f47b4f6fda5fa82bf1ea56
talal <ta440237@gmail.com>
2023-06-03 15:24:10 +0300

    commit message

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+
+function superbowlWin(record) {
+    const winObject = record.find(obj => obj.result === "W");
+    return winObject ? winObject.year : undefined;
+  }
+  
